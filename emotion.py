d86941088448df09b14b56f6fd63e5aebee68cf3
Sayed Afnan Khazi <83779299+Sayed-Afnan-Khazi@users.noreply.github.com>
2022-09-03 15:50:30 +0530

    Initial commit

---
+++ emotion.py
@@ -0,0 +1,41 @@
+import nltk
+nltk.download('punkt')
+import pandas as pd
+from nltk import word_tokenize
+from nltk.stem.snowball import SnowballStemmer
+import requests
+
+from LeXmo import LeXmo
+
+def getEmotion(text):
+
+
+    text = "Like did I do a mistake by messaging them?" 
+
+    emo=LeXmo.LeXmo(text)
+
+    # print(emo)
+
+    maxm = 'anger'
+    for x in ['anticipation', 'disgust', 'fear', 'joy',  'sadness', 'surprise']:
+        if emo[x] > emo[maxm]:
+            maxm = x
+    else:
+        mostProbableEmo = maxm
+
+    maxm = 'negative'
+    for x in ['positive',  'trust']:
+        if emo[x] > emo[maxm]:
+            maxm = x
+    else:
+        mostProbableExtraEmo = maxm
+
+    return mostProbableEmo, mostProbableExtraEmo
+
+if __name__ == "__main__":
+    # testing the function
+    text = "Like did I do a mistake by messaging them?" 
+    mostProbableEmo, mostProbableExtraEmo = getEmotion(text)
+    print("Most Probable Emotion:", mostProbableEmo)
+    print("Most Probable State:", mostProbableExtraEmo)
+
